19aaaf39f02a118401c6a4fb469545c28768a8f4
Vulcan <vulcan.lee@gmail.com>
2023-06-22 19:58:35 +0800

    建立 ConverterToTextFactory

---
+++ Src/GptKM/GptLibrary/Converts/ConverterToTextFactory.cs
@@ -0,0 +1,45 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Text;
+
+namespace GptLibrary.Converts
+{
+    public class ConverterToTextFactory 
+    {
+        public IFileToText Create(string fileType)
+        {
+            IFileToText result = null;
+
+            if (fileType.EndsWith(".xlsx"))
+            {
+                result = new ExcelToText();
+            }
+            else if (fileType.EndsWith(".docx"))
+            {
+                result = new WordToText();
+            }
+            else if (fileType.EndsWith(".pptx"))
+            {
+                result = new PptToText();
+            }
+            else if (fileType.EndsWith(".pdf"))
+            {
+                result = new PdfToText();
+            }
+            else if (fileType.EndsWith(".html"))
+            {
+                result = new HtmlToText();
+            }
+            else if (fileType.EndsWith(".txt"))
+            {
+                result = new TextToText();
+            }
+            else if (fileType.EndsWith(".md"))
+            {
+                result = new MarkdownToText();
+            }
+
+            return result;
+        }
+    }
+}
