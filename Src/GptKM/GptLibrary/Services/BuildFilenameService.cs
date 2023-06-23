b45a9fc0651c5506c4c569fa553634e0f8a3d3f0
Vulcan <vulcan.lee@gmail.com>
2023-06-24 00:29:09 +0800

    使用 GptConstant 來替代

--- Src/GptKM/GptLibrary/Services/BuildFilenameService.cs
+++ Src/GptKM/GptLibrary/Services/BuildFilenameService.cs
@@ -1,20 +1,22 @@
-﻿namespace GptLibrary.Services;
+﻿using GptLibrary.Models;
+
+namespace GptLibrary.Services;
 
 public class BuildFilenameService
 {
     public string BuildConvertedText(string fileName)
     {
-        var newFilePath = $"{fileName}.ConvertedText";
+        var newFilePath = $"{fileName}{GptConstant.ConvertToTextFileExtension}";
         return newFilePath;
     }
     public string BuildEmbeddingText(string fileName, int index)
     {
-        var newFilePath = $"{fileName}.{index}.EmbeddingText";
+        var newFilePath = $"{fileName}.{index}{GptConstant.ConvertToEmbeddingTextFileExtension}";
         return newFilePath;
     }
     public string BuildEmbeddingJson(string fileName, int index)
     {
-        var newFilePath = $"{fileName}.{index}.EmbeddingJson";
+        var newFilePath = $"{fileName}.{index}{GptConstant.ConvertToEmbeddingJsonFileExtension}";
         return newFilePath;
     }
 }
