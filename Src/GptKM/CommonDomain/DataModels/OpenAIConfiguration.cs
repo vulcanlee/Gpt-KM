02b136a1bc6bbbddd9d5dbaa6b9517458b6b7e52
Vulcan <vulcan.lee@gmail.com>
2023-07-18 18:08:21 +0800

    .

--- Src/GptKM/CommonDomain/DataModels/OpenAIConfiguration.cs
+++ Src/GptKM/CommonDomain/DataModels/OpenAIConfiguration.cs
@@ -3,8 +3,8 @@
     public class OpenAIConfiguration
     {
         public string AzureOpenAIKey { get; set; } = string.Empty;
-        public string TextEmbeddingAdaModelName { get; set; } = string.Empty;
-        public string AzureOpenAIEndpoint { get; set; } = string.Empty;
-        public string TextDavinciModelName { get; set; } = string.Empty;
+        public string TextEmbeddingAdaModelName { get; set; } = "text-embedding-ada-002";
+        public string AzureOpenAIEndpoint { get; set; } = "https://openailabtw.openai.azure.com/";
+        public string TextDavinciModelName { get; set; } = "text-davinci-003";
     }
 }
