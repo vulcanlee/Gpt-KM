291b7674654fc2e14868fdb1030b505072170f8b
Vulcan <vulcan.lee@gmail.com>
2023-08-29 21:26:30 +0800

    TwcsAdaEmbeddingVector

--- Src/台智雲/台智雲LLM福爾摩沙大模型 01 Embedding 測試/台智雲LLM福爾摩沙大模型 01 Embedding 測試/Program.cs
+++ Src/台智雲/台智雲LLM福爾摩沙大模型 01 Embedding 測試/台智雲LLM福爾摩沙大模型 01 Embedding 測試/Program.cs
@@ -7,37 +7,42 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace 台智雲LLM福爾摩沙大模型_01_Embedding_測試
 {
-    public class EmbeddingRequest
+    public class EmbeddingTwccRequest
     {
         public List<string> input { get; set; } = new();
     }
 
-    public class EmbeddingResponse
+    public class EmbeddingTwccResponse
     {
-        public List<EmbeddingNodeResponse> Data { get; set; } = new();
+        public List<EmbeddingTwccResponseNode> Data { get; set; } = new();
     }
-    public class EmbeddingNodeResponse
+    public class EmbeddingTwccResponseNode
     {
         public List<float> Embedding { get; set; } = new();
         public int index { get; set; }
     }
-
-    public class Twcs台智雲LLM福爾摩沙大模型_Embedding
+    public interface IAdaEmbeddingVector
     {
-        private readonly ILogger<Twcs台智雲LLM福爾摩沙大模型_Embedding> logger;
+        Task<float[]> GetEmbeddingAsync(string doc);
+    }
+
+    public class TwcsAdaEmbeddingVector: IAdaEmbeddingVector
+    {
+        private readonly ILogger<TwcsAdaEmbeddingVector> logger;
         private readonly IHttpClientFactory httpClientFactory;
         string API_KEY = "9114aa97-8ede-4f8b-8c65-d713223fe090";
         string API_SERVER = "https://ffm-trial05.twcc.ai/embeddings/api/embeddings";
 
-        public Twcs台智雲LLM福爾摩沙大模型_Embedding(ILogger<Twcs台智雲LLM福爾摩沙大模型_Embedding> logger,
+        public TwcsAdaEmbeddingVector(ILogger<TwcsAdaEmbeddingVector> logger,
             IHttpClientFactory httpClientFactory)
         {
             this.logger = logger;
             this.httpClientFactory = httpClientFactory;
         }
 
-        public async Task Run()
+        public async Task<float[]> GetEmbeddingAsync(string doc)
         {
+            List<float> result = new();
             try
             {
                 logger.LogInformation("台智雲 Embedding 測試用服務啟動");
@@ -46,7 +51,7 @@ namespace 台智雲LLM福爾摩沙大模型_01_Embedding_測試
                 var client = httpClientFactory.CreateClient();
                 client.DefaultRequestHeaders.Add("X-API-KEY", API_KEY);
                 client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
-                EmbeddingRequest embeddingRequest = new EmbeddingRequest();
+                EmbeddingTwccRequest embeddingRequest = new EmbeddingTwccRequest();
                 embeddingRequest.input.Add("我想要買一台筆電");
                 var JsonContent = JsonConvert.SerializeObject(embeddingRequest);
                 using (var fooContent = new StringContent(JsonContent, Encoding.UTF8, "application/json"))
@@ -60,7 +65,53 @@ namespace 台智雲LLM福爾摩沙大模型_01_Embedding_測試
                     {
                         // 取得呼叫完成 API 後的回報內容
                         String strResult = await response.Content.ReadAsStringAsync();
-                        EmbeddingResponse embeddingResponse = JsonConvert.DeserializeObject<EmbeddingResponse>(strResult);
+                        EmbeddingTwccResponse embeddingResponse = JsonConvert.DeserializeObject<EmbeddingTwccResponse>(strResult);
+                        result = embeddingResponse.Data[0].Embedding;
+                        logger.LogInformation($"呼叫結果: {strResult}");
+                    }
+                    else
+                    {
+                        logger.LogInformation($"API 異常狀態: " +
+                            string.Format("Error Code:{0}, Error Message:{1}", response.StatusCode, response.RequestMessage));
+                    }
+                }
+                else
+                {
+                    logger.LogInformation($"應用程式呼叫 API 發生異常");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation($"發生例外異常 : {ex.Message}");
+            }
+            return result.ToArray();
+        }
+
+        public async Task Run()
+        {
+            try
+            {
+                logger.LogInformation("台智雲 Embedding 測試用服務啟動");
+
+                HttpResponseMessage response = null;
+                var client = httpClientFactory.CreateClient();
+                client.DefaultRequestHeaders.Add("X-API-KEY", API_KEY);
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+                EmbeddingTwccRequest embeddingRequest = new EmbeddingTwccRequest();
+                embeddingRequest.input.Add("我想要買一台筆電");
+                var JsonContent = JsonConvert.SerializeObject(embeddingRequest);
+                using (var fooContent = new StringContent(JsonContent, Encoding.UTF8, "application/json"))
+                {
+                    response = await client.PostAsync(API_SERVER, fooContent);
+                }
+
+                if (response != null)
+                {
+                    if (response.IsSuccessStatusCode == true)
+                    {
+                        // 取得呼叫完成 API 後的回報內容
+                        String strResult = await response.Content.ReadAsStringAsync();
+                        EmbeddingTwccResponse embeddingResponse = JsonConvert.DeserializeObject<EmbeddingTwccResponse>(strResult);
                         logger.LogInformation($"呼叫結果: {strResult}");
                     }
                     else
@@ -89,7 +140,7 @@ namespace 台智雲LLM福爾摩沙大模型_01_Embedding_測試
                            .ConfigureServices((hostContext, services) =>
                            {
                                services.AddHttpClient();
-                               services.AddTransient<Twcs台智雲LLM福爾摩沙大模型_Embedding>();
+                               services.AddTransient<TwcsAdaEmbeddingVector>();
                            }).UseConsoleLifetime()
                            .ConfigureLogging(logging =>
                            {
@@ -105,7 +156,7 @@ namespace 台智雲LLM福爾摩沙大模型_01_Embedding_測試
 
                 try
                 {
-                    var myService = services.GetRequiredService<Twcs台智雲LLM福爾摩沙大模型_Embedding>();
+                    var myService = services.GetRequiredService<TwcsAdaEmbeddingVector>();
                     await myService.Run();
                 }
                 catch (Exception ex)
